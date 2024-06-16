db3f84b4a800a29b1ef41ce8070a7690a26340c2
timsu92 <33785401+timsu92@users.noreply.github.com>
2024-06-16 22:03:49 +0800

    feat: send purchase data once a second

--- backend/controllers/purchase.js
+++ backend/controllers/purchase.js
@@ -1,12 +1,22 @@
 const sendToKafka = require('../utils/sendToKafka');
 const wss = require('./websocketServer_Counter1');
 const WebSocket = require('ws');
+
 let count = 0;
+
+// 將資料傳送給所有連接的 WebSocket 客戶端
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(count);
+    }
+  });
+}, 1000);
+
 /**
 * @typedef {Object} Order
 * @property {string} name 購買者名稱
 */
-
 
 /**
 * 讓使用者購買商品，並且會需要將購買事實傳送給 Kafka
@@ -23,20 +33,13 @@ module.exports = async function purchase(req, res) {
   console.log(`${req.body.name} 買了商品`);
   // TODO: 這裡會需要去增加「API接收到請求」的計數器
   // TODO: 這裡會跟 Kafka 溝通
-const data = JSON.stringify({
+  const data = JSON.stringify({
     buy_name: req.body.name,
     buy_time: Math.floor(now / 1000)
   });
   try {
-    count ++; 
+    count++;
     await sendToKafka.sendToKafka(data);
-
-    // 將資料傳送給所有連接的 WebSocket 客戶端
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(count);
-      }
-    });
 
     return res.status(200).send('OK');
   } catch (error) {
